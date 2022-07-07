bf3c1464f55057ce1171c5151432e484809331cb
Jeff Modzel <jeff.modzel@voicefoundry.com>
2022-07-07 13:44:16 -0400

    add shell again again

--- lib/ShellAppStage.ts
+++ lib/ShellAppStage.ts
@@ -5,6 +5,8 @@ export class ShellAppStage extends cdk.Stage {
     
     constructor(scope: Construct, id: string, props?: cdk.StageProps) {
       super(scope, id, props);
-      new cdk.CfnOutput(this, 'Output', {value: 'value'});
+      //new cdk.CfnOutput(this, 'Output', {value: 'value'});
+      const stack = new cdk.Stack(this, 'empty-stack');
+
     }
 }
